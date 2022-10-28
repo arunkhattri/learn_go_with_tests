db3d3ae4f4a3f2b2b923337929b3e54f4786b5de
Arun Kr. Khattri <arunkhattri@outlook.com>
2022-10-28 12:51:04 +0530

    refactored hello and test_hello

--- 01_hello_world/hello.go
+++ 01_hello_world/hello.go
@@ -2,14 +2,20 @@ package main
 
 import "fmt"
 
+const englishHelloPrefix = "Hello, "
+
 func Hello() string {
 	return "Hello, world"
 }
 
 func Greet(name string) string {
-	return "Hello, " + name
+	if name == "" {
+		name = "World"
+	}
+	return englishHelloPrefix + name
 }
 
 func main() {
 	fmt.Println(Hello())
+	fmt.Println(Greet("Chris"))
 }
